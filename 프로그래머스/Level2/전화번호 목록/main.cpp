872c8d4c07c08b9d2b2deb6c78b2cc4e0acaa27f
LeeKyuTae <crown0403@khu.ac.kr>
2020-02-01 13:40:14 +0900

    정력방식과 우선순위 큐 방식 추가

--- 프로그래머스/Level2/전화번호 목록/main.cpp
+++ 프로그래머스/Level2/전화번호 목록/main.cpp
@@ -1,10 +1,14 @@
 #include <string>
 #include <vector>
-
+#include <algorithm>
+#include <queue>
 using namespace std;
 
 bool solution(vector<string> phone_book) {
     bool answer = true;
+    
+    //내 첫번째 풀이
+    /*
     for(int i=0; i<phone_book.size(); i++){
         string standard = phone_book[i];
         int standardSize = standard.size();
@@ -21,6 +25,36 @@ bool solution(vector<string> phone_book) {
         }
         if(answer == false)
             break;
+    }
+    */ 
+    
+    //두번째 방법
+    /*
+    sort(phone_book.begin(), phone_book.end());
+    for(int i=0; i<phone_book.size()-1; i++){
+        if(phone_book[i]==phone_book[i+1].substr(0, phone_book[i].size())){
+            answer = false;
+            break;
+        }
+    }
+    */
+    
+    priority_queue<string, vector<string>, greater<string>> q;
+    for(int i=0; i<phone_book.size(); i++){
+        q.push(phone_book[i]);
+    }
+    while(!q.empty()){
+        string standard = q.top();
+        q.pop();
+        if(q.size()==0){
+            break;
+        }
+        string value = q.top();
+        q.pop();
+        if(standard == value.substr(0, standard.size())){
+            answer = false;
+            break;
+        }
     }
     return answer;
 }
